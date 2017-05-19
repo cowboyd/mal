cc5dc0e631d2ee6e980197874cc7a2c22fc383c0
Charles Lowell <cowboyd@frontside.io>
2017-05-19 17:35:46 -0500

    first stab at step0

--- node/step0_repl.js
+++ node/step0_repl.js
@@ -1,1 +1,29 @@
+const readline = require('readline');
 
+function READ(string) {
+  return string;
+}
+
+function EVAL(string) {
+  return string;
+}
+
+function PRINT(string) {
+  return string;
+}
+
+function rep(string) {
+  return PRINT(EVAL(READ(string)));
+}
+
+let io = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "user> "
+});
+
+io.prompt();
+io.on('line', (line)=> {
+  io.output.write(rep(line) + "\n");
+  io.prompt();
+});
